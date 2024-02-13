de7d8eac7cff484d8d1af34d13d843dedae070c2
Vibhash <vibhashgyanminz23@gmail.com>
2024-02-13 09:14:54 +0530

    arrow function

--- refresher/basics.js
+++ refresher/basics.js
@@ -1,6 +1,9 @@
 const name = "Vic"; // value cant be change
 let age = 28; // value can be change
 const isActive = true;
+let phoneNumber = 1234567890;
+let countrycode = "+91";
+const nationality = "Indian";
 
 age = 27;
 
@@ -15,4 +18,24 @@ function summerizeUser(userName, userAge, userActive) {
   );
 }
 
+///Arrow function
+
+///const sum = (a,b,c)=> a+b+c;
+const userDetail = function (userName, userAge, phone, code, nationality) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the phone number is " +
+    code +
+    "-" +
+    phone +
+    " has " +
+    nationality +
+    " citizenship"
+  );
+};
+
 console.log(summerizeUser(name, age, isActive));
+console.log(userDetail(name, age, phoneNumber, countrycode, nationality));
